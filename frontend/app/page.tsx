5924a804d2cbb53e23f322b22ff543fe06aa86dd
z3y <z3y@protonmail.com>
2024-07-24 16:18:17 +0200

    search

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,33 +2,16 @@
 
 import PrefabData, { prefabTestData } from "@/components/Prefab"
 import PrefabListingFull from "@/components/PrefabListingFull"
-import SearchOptions from "@/components/SearchOptions"
-import { SetStateAction, useState } from "react"
-
-function Main() {
-  return (
-    <div className="justify-center flex ">
-      <div
-        className={
-          "pt-16 pb-16 grid gap-4 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2"
-        }
-      >
-        {prefabTestData.map((x) => (
-          <PrefabListingFull {...x} />
-        ))}
-      </div>
-
-      <div
-        id="search bar"
-        className="fixed bg-opacity-95 backdrop-blur-md bg-zinc-900 rounded-lg top-6 min-h-20 p-4 border-2 border-zinc-600 border-opacity-50 content-center"
-      >
-        <h1>Search</h1>
-      </div>
-    </div>
-  )
-}
+import { useSearchParams } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Home() {
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    console.log(searchParams.toString())
+  }, [searchParams.get("name")])
+
   return (
     <main className="bg-zinc-950 h-full overflow-auto">
       <div
@@ -36,19 +19,11 @@ export default function Home() {
         className="h-full flex flex-row max-w-[1300px] mx-auto"
       >
         <div
-          id="search-options"
-          className="relative min-w-80 border-r border-l border-zinc-800 hidden lg:block"
-        >
-          <div className="fixed min-w-80 p-4">
-            <SearchOptions />
-          </div>
-        </div>
-        <div
           id="prefabs-list"
-          className="grow flex flex-col border-r border-zinc-800"
+          className="grow flex flex-col border-r border-l border-zinc-800"
         >
-          {prefabTestData.map((x) => (
-            <PrefabListingFull {...x} />
+          {prefabTestData.map((x, index) => (
+            <PrefabListingFull {...x} key={index} />
           ))}
         </div>
       </div>
--- frontend/components/SearchOptions.tsx
+++ frontend/components/SearchOptions.tsx
@@ -1,63 +1,89 @@
-import {
-  Input,
-  Listbox,
-  ListboxButton,
-  ListboxOption,
-  ListboxOptions,
-  Select,
-} from "@headlessui/react"
-import React, { useState } from "react"
+"use client"
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import React, { useEffect, useState } from "react"
 
 const SearchOptions = () => {
+  const searchParams = useSearchParams()
+
+  const [searchText, setSearchText] = useState("")
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const previousName = searchParams.get("name")
+    setSearchText(previousName ?? "")
+    console.log("setting " + previousName)
+  }, [])
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const searchParams = new URLSearchParams(window.location.search)
+    searchText && searchParams.set("name", searchText)
+
+    const newPath = `${window.location.pathname}?${searchParams.toString()}`
+
+    router.push(newPath)
+  }
+
   return (
-    <form className="flex flex-col gap-2 pt-1">
-      <Input
-        className="bg-zinc-950 border border-zinc-800 rounded-md p-4 focus:outline-none"
-        type="text"
-        id="search"
-        name="prefab"
-        placeholder="Search"
-      ></Input>
-
-      <div className="flex flex-row items-center gap-4 pt-4">
-        <p className="text-zinc-200 text-sm w-12">Sorting</p>
-
-        <Select
-          name="sorting"
-          aria-label="Popularity"
-          className="bg-zinc-950  border border-zinc-800 rounded-md p-2 focus:outline-none w-56"
-        >
-          <option value="popularity">Popularity</option>
-          <option value="new">New</option>
-          <option value="old">Old</option>
-        </Select>
-      </div>
-
-      <div className="flex flex-row items-center gap-4">
-        <p className="text-zinc-200 text-sm w-12">Type</p>
-        <Select
-          name="type"
-          aria-label="Any"
-          className="bg-zinc-950 border border-zinc-800 rounded-md p-2 focus:outline-none w-56"
-        >
-          <option value="any">Any</option>
-          <option value="udon">Udon</option>
-          <option value="avatar">Avatar</option>
-          <option value="tool">Tool</option>
-          <option value="general">General Assets</option>
-        </Select>
-      </div>
-
-      <div className="flex flex-row items-center gap-4">
-        <p className="text-zinc-200 text-sm w-12">Creator</p>
-        <Input
-          className="bg-zinc-950 border border-zinc-800 rounded-md p-2 focus:outline-none w-56"
+    <form
+      className="flex flex-row gap-2"
+      autoComplete="off"
+      onSubmit={handleSearch}
+    >
+      <div className="">
+        <input
+          className="appearance-none rounded py-2 px-3 text-zinc-200 focus:outline-none text-center text-sm"
           type="text"
-          id="search-creator"
-          name="prefab-creator"
-          placeholder=""
-        ></Input>
+          id="search"
+          name="prefab"
+          placeholder="Search Prefabs"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
       </div>
+
+      {/* <div className="flex flex-row items-center gap-4 pt-4">
+          <p className="text-zinc-200 text-sm w-12">Sorting</p>
+
+          <Select
+            name="sorting"
+            aria-label="Popularity"
+            className="bg-zinc-950  border border-zinc-800 rounded-md p-2 focus:outline-none w-56"
+          >
+            <option value="popularity">Popularity</option>
+            <option value="new">New</option>
+            <option value="old">Old</option>
+          </Select>
+        </div> */}
+
+      {/* <div className="flex flex-row items-center gap-4">
+          <p className="text-zinc-200 text-sm w-12">Type</p>
+          <Select
+            name="type"
+            aria-label="Any"
+            className="bg-zinc-950 border border-zinc-800 rounded-md p-2 focus:outline-none w-56"
+          >
+            <option value="any">Any</option>
+            <option value="udon">Udon</option>
+            <option value="avatar">Avatar</option>
+            <option value="tool">Tool</option>
+            <option value="general">General Assets</option>
+          </Select>
+        </div> */}
+
+      {/* <div className="flex flex-row items-center gap-4">
+          <p className="text-zinc-200 text-sm w-12">Creator</p>
+          <input
+            className="bg-zinc-950 border border-zinc-800 rounded-md p-2 focus:outline-none w-56"
+            type="text"
+            id="creator"
+            name="creator"
+            placeholder=""
+          ></input>
+        </div> */}
     </form>
   )
 }
