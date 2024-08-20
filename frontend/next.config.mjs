99a61ab8338fc18d3ffe4aba2278b2cc04f114b2
z3y <z3y@protonmail.com>
2024-08-20 17:09:13 +0200

    update next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+}
+
+// https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
+
+  nextConfig.assetPrefix = `/${repo}`
+  nextConfig.basePath = `/${repo}`
 }
 
 export default nextConfig
